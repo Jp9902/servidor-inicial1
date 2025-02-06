1cb4e3d25f70218aa687572f18b68df8139eb278
João Pedro Martins Kazakevicius <jplpd013@gmail.com>
2025-02-06 12:40:55 -0300

    ajuste de erros e enviando resposta

--- script.js
+++ script.js
@@ -10,12 +10,21 @@ const port = 3000;
 //Criando servidor usando função com nome diferente
 const meuServidor = http.createServer((req,res)=> {
 
+
+
+//Configurando o cabeçalho de respostas
+res.statusCode = 200;
+res.setHeader('Content-type', 'text/plain');
+
+//enviando mensagem de resposta
+res.end('Bem vindos ao server inicial 1!!')
+
 })
 
 
-//criando o servidor
-res.statusCode = 200;
-res.setHeader('Content-type', 'text/plain');
+
+
+
 
 
 
