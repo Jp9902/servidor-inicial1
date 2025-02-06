a751b37c86a16de67792d4d16848fe19c9b9899f
João Pedro Martins Kazakevicius <jplpd013@gmail.com>
2025-02-06 12:33:58 -0300

    configurando

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //importando o módulo HTTP
-const http = require('http')
+const http = require('http');
+
+
+//Configurando as informações do servidor
+const Hostname = '127.0.0.1';
+const port = 3000;
+
